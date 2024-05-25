0c7523fc46462eaac2542988ada7f9f9decd4960
Vladimir Cvetic <vladimir@ferdinand.rs>
2024-05-25 09:25:47 +0200

    build mongo

--- mongoDB/run.sh
+++ mongoDB/run.sh
@@ -11,14 +11,13 @@ rm -rf /tmp/bashio
 MONGO_INITDB_ROOT_USERNAME=$(bashio::config 'MONGO_INITDB_ROOT_USERNAME')
 MONGO_INITDB_ROOT_PASSWORD=$(bashio::config 'MONGO_INITDB_ROOT_PASSWORD')
 MONGO_PORT=$(bashio::config 'MONGO_PORT')
-MONGO_BIND_IP=$(bashio::config 'MONGO_BIND_IP')
 
 if [ -z "$MONGO_INITDB_ROOT_USERNAME" ] || [ -z "$MONGO_INITDB_ROOT_PASSWORD" ]; then
   echo "MongoDB root username or password is not set. Exiting."
   exit 1
 fi
 
-mongod --auth --bind_ip $MONGO_BIND_IP --port $MONGO_PORT &
+mongod --auth --port $MONGO_PORT &
 mongo_admin="mongo admin -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --port $MONGO_PORT"
 until $mongo_admin --eval "print(\"waited for connection\")"; do
   >&2 echo "MongoDB is unavailable - sleeping"
