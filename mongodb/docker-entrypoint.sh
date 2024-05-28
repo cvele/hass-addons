03bbfbab47054aa0515c5081f798a0f81a1e613f
Vladimir Cvetic <vladimir@ferdinand.rs>
2024-05-28 11:53:48 +0200

    Initial release (#2)

--- mongodb/docker-entrypoint.sh
+++ mongodb/docker-entrypoint.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-source /usr/lib/bashio/bashio.sh
-export MONGO_INITDB_ROOT_USERNAME=$(bashio::config 'MONGO_INITDB_ROOT_USERNAME')
-export MONGO_INITDB_ROOT_PASSWORD=$(bashio::config 'MONGO_INITDB_ROOT_PASSWORD')
-
-if [ -z "$MONGO_INITDB_ROOT_USERNAME" ] || [ -z "$MONGO_INITDB_ROOT_PASSWORD" ]; then
-  echo "MongoDB root username or password is not set. Exiting."
-  exit 1
+# Source bashio
+if ! source /usr/lib/bashio/bashio.sh; then
+    echo "Failed to source bashio. Exiting."
+    exit 1
 fi
 
-if [ "${1:0:1}" = '-' ]; then
-	set -- mongod "$@"
+# Retrieve configuration from bashio
+MONGO_INITDB_ROOT_USERNAME=$(bashio::config 'MONGO_INITDB_ROOT_USERNAME')
+MONGO_INITDB_ROOT_PASSWORD=$(bashio::config 'MONGO_INITDB_ROOT_PASSWORD')
+
+# Check if the required environment variables are set
+if [ -z "${MONGO_INITDB_ROOT_USERNAME}" ] || [ -z "${MONGO_INITDB_ROOT_PASSWORD}" ]; then
+    echo "MongoDB root username or password is not set. Exiting."
+    exit 1
+fi
+
+# Add additional arguments to the command if provided
+if [[ "${1:0:1}" = '-' ]]; then
+    set -- mongod "$@"
+fi
+
+# Ensure the mongod command runs properly
+if [[ "$1" == "mongod" ]]; then
+    exec "$@" --bind_ip_all --port 27017 --auth
+else
+    exec "$@"
 fi
 
 originalArgOne="$1"
