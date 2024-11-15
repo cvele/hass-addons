92645f4b14517b37c731dafbd1ca7a2ca5d1665d
Vladimir Cvetic <vladimir@ferdinand.rs>
2024-11-15 11:43:19 +0100

    Bump Miniflux version to 2.2.3-8 and enhance configuration handling in miniflux-run.sh

--- miniflux/miniflux-run.sh
+++ miniflux/miniflux-run.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/with-contenv bash
-
+# Import Bashio
 echo "Starting Miniflux service..."
 
+# Create the Miniflux configuration file
 cat <<EOF > /etc/miniflux.conf
 LOG_DATE_TIME=yes
 LISTEN_ADDR=0.0.0.0:8080
-DATABASE_URL=${DATABASE_URL}
-RUN_MIGRATIONS=${RUN_MIGRATIONS:-0}
+DATABASE_URL=$(bashio::config 'DATABASE_URL')
+RUN_MIGRATIONS=$(bashio::config 'RUN_MIGRATIONS')
+ADMIN_PASSWORD=$(bashio::config 'ADMIN_PASSWORD')
+ADMIN_USERNAME=$(bashio::config 'ADMIN_USERNAME')
 EOF
 
+# Log the generated configuration for debugging
+bashio::log.info "Generated Miniflux configuration:"
+cat /etc/miniflux.conf
+
+# Start Miniflux
 exec miniflux -c /etc/miniflux.conf
