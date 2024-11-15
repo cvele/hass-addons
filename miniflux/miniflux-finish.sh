f29f19033a50c1cb104f404bd82e5efd506a0584
Vladimir Cvetic <vladimir@ferdinand.rs>
2024-11-15 11:39:50 +0100

    Bump Miniflux version to 2.2.3-7 and update configuration handling

--- miniflux/miniflux-finish.sh
+++ miniflux/miniflux-finish.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/with-contenv bash
 # /etc/services.d/miniflux/finish
 
+rm -rf /etc/miniflux.conf
 echo "Stopping Miniflux service..."
--- miniflux/miniflux-run.sh
+++ miniflux/miniflux-run.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/with-contenv bash
-# /etc/services.d/miniflux/run
 
 echo "Starting Miniflux service..."
-exec miniflux
+
+cat <<EOF > /etc/miniflux.conf
+LOG_DATE_TIME=yes
+LISTEN_ADDR=0.0.0.0:8080
+DATABASE_URL=${DATABASE_URL}
+RUN_MIGRATIONS=${RUN_MIGRATIONS:-0}
+EOF
+
+exec miniflux -c /etc/miniflux.conf
